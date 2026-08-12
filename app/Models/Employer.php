b465e6ac3f98d6d8ad3c609c49caad0fab4ec006
Shakshor Sarker <shakshor584@gmail.com>
2026-08-13 00:21:15 +0600

    added relation among job, employer and user

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    // belongs to user
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // has many jobs
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class);
+    }
 }
